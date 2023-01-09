4417a942b0b69a871179be5b087cd3c7bff9207c
indraindrajit71 <45243018+indraindrajit71@users.noreply.github.com>
2023-01-09 11:42:04 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,4 @@ echo "************************************************"
 echo "*****Building-Jar*************"
 echo "************************************************"
 
-docker run -it -v $PWD/java-app:/app -v /root/.m2:/root/.m2  -w /app maven:3-alpine "$@"
+docker run -v $PWD/java-app:/app -v /root/.m2:/root/.m2  -w /app maven:3-alpine "$@"
